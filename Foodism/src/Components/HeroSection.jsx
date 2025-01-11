e7aa378601b3e55770dbe7412c925a3ed9d1c070
anikaivnath <anikaivnath8@gmail.com>
2025-01-11 23:49:27 +0600

    fix(HeroSection): adjust paragraph styling for improved readability

--- Foodism/src/Components/HeroSection.jsx
+++ Foodism/src/Components/HeroSection.jsx
@@ -5,7 +5,7 @@ const HeroSection = () => {
     <div>
     <div className='  text-justify'>
         <h1 className='text-3xl font-bold gap-y-4 w-[280px]' >Dive into Delights Of Delectable <span className='text-green-400'>Food</span> </h1>
-        <p>Where Each Plate Weaves a Story of Culinary Mastery and Passionate Craftsmanship</p>
+        <p className=''>Where Each Plate Weaves a Story of Culinary Mastery and Passionate Craftsmanship</p>
     </div>
 
     </div>

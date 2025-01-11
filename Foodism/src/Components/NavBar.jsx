9c10ac3a821992f2a16364b2f56530e04411e8db
anikaivnath <anikaivnath8@gmail.com>
2025-01-11 23:06:08 +0600

    style(NavBar): improve layout and spacing with flexbox adjustments and enhance text styling for navigation items

--- Foodism/src/Components/NavBar.jsx
+++ Foodism/src/Components/NavBar.jsx
@@ -5,15 +5,14 @@ import { faCartShopping, faEnvelope, faPhone, faSearch } from '@fortawesome/free
 const NavBar = () => {
   return (
     <div>
-        <div className='flex'>
+        <div className='flex justify-around'>
         <div>
-        <h1 className='text-4xl font-bold '>
-            <span className='bg-green-400 w-max  text-white rounded-xl' >F</span>oodism
+        <h1 className='text-4xl font-bold mt-2 '>
+            <span className='bg-green-400 w-max  text-white rounded-xl px-2' >F</span>oodism
         </h1>
-
         </div>
 
-        <div className=' w-80 flex justify-around text-xl '>
+        <div className=' w-80 flex justify-around text-xl mt-5 '>
         <p className='text-green-600'>Home</p>
         <p>Menu</p>
         <p>Services</p>
@@ -21,10 +20,10 @@ const NavBar = () => {
 
         </div>
 
-        <div className='flex flex-row justify-around w-40'> 
-        <i> <FontAwesomeIcon icon={faSearch} /></i>
-        <i> <FontAwesomeIcon icon={faCartShopping} /></i>
-        <i><FontAwesomeIcon icon={faPhone}/></i>
+        <div className='flex flex-row justify-around w-40 mt-5 cursor-pointer'> 
+        <i className='text-xl'> <FontAwesomeIcon icon={faSearch} /></i>
+        <i className='text-xl'> <FontAwesomeIcon icon={faCartShopping} /></i>
+        <i className='text-xl text-white bg-green-400 px-4 rounded-xl'><FontAwesomeIcon icon={faPhone}/></i>
 
         </div>
         

31f46e7326a2cfae22148c375982a6defe5a4ac2
anikaivnath <anikaivnath8@gmail.com>
2025-01-11 21:40:49 +0600

    replace Home component with NavBar in App and add NavBar component

--- Foodism/src/App.jsx
+++ Foodism/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Home from './Components/Home'
+import NavBar from './Components/NavBar'
 
 const App = () => {
   return (
     <div>
-    <Home/>
+    <NavBar/>
 
     </div>
   )
---
+++ Foodism/src/Components/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>
+        <div>
+        <div>
+        <h1>
+            <span >F</span>oodism
+        </h1>
+
+        </div>
+
+    
+        </div>
+    </div>
+  )
+}
+
+export default NavBar

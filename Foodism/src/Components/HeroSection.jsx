03874e6a5637b67ff22c67154d30a0f1145f2cee
anikaivnath <anikaivnath8@gmail.com>
2025-01-11 23:51:13 +0600

    fix(HeroSection): update styling for improved layout and spacing

--- Foodism/src/Components/HeroSection.jsx
+++ Foodism/src/Components/HeroSection.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const HeroSection = () => {
   return (
     <div>
-    <div className='  text-justify'>
-        <h1 className='text-3xl font-bold gap-y-4 w-[280px]' >Dive into Delights Of Delectable <span className='text-green-400'>Food</span> </h1>
-        <p className=''>Where Each Plate Weaves a Story of Culinary Mastery and Passionate Craftsmanship</p>
+    <div className='  text-justify m-40 '>
+        <h1 className='text-3xl font-bold  w-[280px]' >Dive into Delights Of Delectable <span className='text-green-400'>Food</span> </h1>
+        <p className='w-[320px] mt-4'>Where Each Plate Weaves a Story of Culinary Mastery and Passionate Craftsmanship</p>
     </div>
 
     </div>

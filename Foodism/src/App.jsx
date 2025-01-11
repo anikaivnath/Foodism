1dfa878162540a02ee430a77efde872952fd4c24
anikaivnath <anikaivnath8@gmail.com>
2025-01-11 23:30:42 +0600

    feat(App): integrate HeroSection component into the main application layout

--- Foodism/src/App.jsx
+++ Foodism/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from './Components/NavBar'
+import HeroSection from './Components/HeroSection'
 
 const App = () => {
   return (
     <div>
     <NavBar/>
+    <HeroSection/>
 
     </div>
   )

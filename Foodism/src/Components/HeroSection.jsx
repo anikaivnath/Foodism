dda8963e7a76401f3df1c3dd012f4457dc56c699
anikaivnath <anikaivnath8@gmail.com>
2025-01-11 23:41:22 +0600

    feat(HeroSection): enhance content and styling with descriptive text and improved layout

--- Foodism/src/Components/HeroSection.jsx
+++ Foodism/src/Components/HeroSection.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const HeroSection = () => {
   return (
     <div>
-    <div>
-        <h1>Dive into Delights Of Delectable Food</h1>
+    <div className='  text-justify'>
+        <h1 className='text-3xl font-bold gap-y-4 w-[280px]' >Dive into Delights Of Delectable <span className='text-green-400'>Food</span> </h1>
+        <p>Where Each Plate Weaves a Story of Culinary Mastery and Passionate Craftsmanship</p>
     </div>
 
     </div>

928bf6a3fe3de4c0c132add89165dc9012507d66
anikaivnath <anikaivnath8@gmail.com>
2025-01-12 18:11:54 +0600

    fix(HeroSection): adjust image positioning for improved layout consistency

--- Foodism/src/Components/HeroSection.jsx
+++ Foodism/src/Components/HeroSection.jsx
@@ -30,8 +30,8 @@ const HeroSection = () => {
 
       {/* Right Section */}
       <div className='relative'>
-        <img className='absolute mt-32  w-[350px] hidden md:block' src={Rectangle} alt="background" />
-        <img className='w-[300px] md:w-[400px] relative ' src={Intersect} alt="picture" />
+        <img className='absolute mt-24 ml-10  w-[350px] hidden md:block' src={Rectangle} alt="background" />
+        <img className='w-[300px] md:w-[400px] relative' src={Intersect} alt="picture" />
       </div>
     </div>
   );

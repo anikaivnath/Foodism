807142cbcc715863ec9973bc9d14407f0d37dd26
anikaivnath <anikaivnath8@gmail.com>
2025-01-12 14:14:18 +0600

    add picture

--- Foodism/src/Components/HeroSection.jsx
+++ Foodism/src/Components/HeroSection.jsx
@@ -7,14 +7,13 @@ const HeroSection = () => {
     <div className='  text-justify ml-32 mt-32 '>
         <h1 className='text-3xl font-bold  w-[280px]' >Dive into Delights Of Delectable <span className='text-green-400'>Food</span> </h1>
         <p className='w-[320px] mt-4'>Where Each Plate Weaves a Story of Culinary Mastery and Passionate Craftsmanship</p>
-  
     <div className='flex gap-4'>
         <p className='bg-green-600 w-max p-2 text-white rounded-2xl shadow-3xl mt-2'>Order Now</p>
         <p className='bg-green-600 w-max p-2 text-white rounded-2xl  shadow-3xl mt-2'>Contact us</p>
     </div>
     </div>
     <div>
-    <img src={Intersect} alt="picture" />
+    <img className='w-80' src={Intersect} alt="picture" />
 
 
     </div>

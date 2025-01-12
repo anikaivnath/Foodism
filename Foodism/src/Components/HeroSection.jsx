8bb6dde8276f70ec606595f56fae47f354fc5f8a
anikaivnath <anikaivnath8@gmail.com>
2025-01-12 16:58:05 +0600

    feat(HeroSection): enhance button styling and add icons for improved user interaction

--- Foodism/src/Components/HeroSection.jsx
+++ Foodism/src/Components/HeroSection.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping, faPhone, faSearch } from '@fortawesome/free-solid-svg-icons'
+
 import Intersect from "./Intersect.png";
 
 const HeroSection = () => {
@@ -8,8 +11,8 @@ const HeroSection = () => {
         <h1 className='text-3xl font-bold  w-[280px]' >Dive into Delights Of Delectable <span className='text-green-400'>Food</span> </h1>
         <p className='w-[320px] mt-4'>Where Each Plate Weaves a Story of Culinary Mastery and Passionate Craftsmanship</p>
     <div className='flex gap-4'>
-        <p className='bg-green-600 w-max p-2 text-white rounded-2xl shadow-3xl mt-2'>Order Now</p>
-        <p className='bg-green-600 w-max p-2 text-white rounded-2xl  shadow-3xl mt-2'>Contact us</p>
+        <p className='bg-green-600 w-max pl-4 pr-4 text-center  text-white rounded-2xl shadow-3xl mt-2'>Order Now</p>
+        <p className='bg-green-600 w-max p-2 text-white rounded-2xl  shadow-3xl mt-2'>  <i className='p-2'> <FontAwesomeIcon icon={faPhone} /></i>Contact us</p>
     </div>
     </div>
     <div>

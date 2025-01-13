1769acd3086b95a0eec8a508ce20568209a3b524
anikaivnath <anikaivnath8@gmail.com>
2025-01-13 15:22:22 +0600

    feat(Menu): add Burger image and improve structure in Menu component

--- Foodism/src/Components/Menu.jsx
+++ Foodism/src/Components/Menu.jsx
@@ -1,23 +1,17 @@
-import React from 'react'
+import React from 'react';
+import Burger from './image/Burger.png';  
 
 const Menu = () => {
   return (
     <div>
-    <div className='text-center m-4 font-custom'> 
-      <h1 className='text-xl'>Satisfy your cravings with food made to lift your mood</h1>
+      <div className='text-center m-4 font-custom'> 
+        <h1 className='text-xl'>Satisfy your cravings with food made to lift your mood</h1>
+      </div>
+      <div>
+        <img src={Burger} alt="Burger" />
+      </div>
     </div>
-    <div>
-    
-    <div>
-      
-    </div>
+  );
+};
 
-
-    </div>
-
-    </div>
-    
-  )
-}
-
-export default Menu
+export default Menu;

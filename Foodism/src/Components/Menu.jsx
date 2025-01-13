c5d3600f790617e22e0e058601b1ade3d45e165a
anikaivnath <anikaivnath8@gmail.com>
2025-01-13 13:20:53 +0600

    feat(Menu): update Menu component styling and add custom font support

--- Foodism/src/Components/Menu.jsx
+++ Foodism/src/Components/Menu.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Menu = () => {
   return (
-    <div>
+    <div className='text-center mt-10  font-custom'>
       <h1>Welcome to the Menu Page!</h1>
       <p>Browse our delicious menu options here.</p>
     </div>

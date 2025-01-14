d249075101c6e167241e062a0e840b71a4abfb05
anikaivnath <anikaivnath8@gmail.com>
2025-01-15 00:24:42 +0600

    fix(ReviewSection): remove unnecessary whitespace for cleaner code

--- Foodism/src/Components/ReviewSection.jsx
+++ Foodism/src/Components/ReviewSection.jsx
@@ -6,14 +6,10 @@ const ReviewSection = () => {
   return (
     <div>
     <div className='relative'>
-
-            <img className='absolute mt-[266px] ml-4 w-80 hidden md:block' src={Rectang} alt='rectang'/>
-            <img className='w-80 relative' src={Mask} alt="picture" />
-
+       <img className='absolute mt-[266px] ml-4 w-80 hidden md:block' src={Rectang} alt='rectang'/>
+       <img className='w-80 relative' src={Mask} alt="picture" />
             <div className="absolute top-6 left-8 bg-white px-3 py-1 rounded-lg">
             </div>
-
-        
     </div>
     </div>
   )

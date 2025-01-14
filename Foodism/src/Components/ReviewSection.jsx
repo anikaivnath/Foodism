c3b16cb90eb7e4d9d55556ae8a1b0b83b66fd5b8
anikaivnath <anikaivnath8@gmail.com>
2025-01-14 23:49:15 +0600

    feat(ReviewSection): enhance styling of testimonials header for better visibility

--- Foodism/src/Components/ReviewSection.jsx
+++ Foodism/src/Components/ReviewSection.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const ReviewSection = () => {
   return (
     <div>
-        <div>
-            <p>Testimonials</p>
+        <div className='text-center m-4 font-custom'>
+            <p className=' text-red-500  '>Testimonials</p>
+
         </div>
     </div>
   )

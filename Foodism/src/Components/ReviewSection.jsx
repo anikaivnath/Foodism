23e4d43f8d6f5122d894c6c6e2ff4ec241be7032
anikaivnath <anikaivnath8@gmail.com>
2025-01-15 00:21:59 +0600

    fix(ReviewSection): adjust image positioning and size for better layout

--- Foodism/src/Components/ReviewSection.jsx
+++ Foodism/src/Components/ReviewSection.jsx
@@ -6,9 +6,9 @@ const ReviewSection = () => {
   return (
     <div>
     <div className='relative'>
-    
-            <img className='absolute mt-32 ml-10  w-[400px] hidden md:block' src={Rectang} alt='rectang'/>
-            <img className='w-[460px] relative' src={Mask} alt="picture" />
+
+            <img className='absolute mt-[266px] ml-4 w-80 hidden md:block' src={Rectang} alt='rectang'/>
+            <img className='w-80 relative' src={Mask} alt="picture" />
 
             <div className="absolute top-6 left-8 bg-white px-3 py-1 rounded-lg">
             </div>

614bb284891c9c22f8a31abd78483062ac798f9c
anikaivnath <anikaivnath8@gmail.com>
2025-01-15 00:18:09 +0600

    feat(ReviewSection): add background image and adjust layout for improved aesthetics

--- Foodism/src/Components/ReviewSection.jsx
+++ Foodism/src/Components/ReviewSection.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import Mask from './image/Mask.png'
+import Rectang from './image/Rectang.png'
 
 const ReviewSection = () => {
   return (
     <div>
-        <div className='text-center m-4 font-custom'>
-            <p className=' text-red-500 m-4 '>Testimonials</p>
+    <div className='relative'>
+    
+            <img className='absolute mt-32 ml-10  w-[400px] hidden md:block' src={Rectang} alt='rectang'/>
+            <img className='w-[460px] relative' src={Mask} alt="picture" />
 
-        </div>
-        <div>
-          <img className='bg{}' src={Mask} alt="" />
-        </div>
+            <div className="absolute top-6 left-8 bg-white px-3 py-1 rounded-lg">
+            </div>
+
+        
+    </div>
     </div>
   )
 }

6dae40d4c4d2fa0795a0d643b997320ada618ecd
anikaivnath <anikaivnath8@gmail.com>
2025-01-14 21:41:20 +0600

    feat(App): add AboutSection component and integrate it into the main application layout

--- Foodism/src/App.jsx
+++ Foodism/src/App.jsx
@@ -4,18 +4,21 @@ import NavBar from "./Components/NavBar";
 import HeroSection from "./Components/HeroSection";
 import Services from "./Components/Services";
 import FoodRecipes from "./Components/FoodRecipes";
+import AboutSection from "./Components/AboutSection";
 import Menu from "./Components/Menu"; 
 
 const App = () => {
   return (
     <Router>
       <NavBar />
+      
       <Routes>
         <Route path="/" element={<><HeroSection /></>} />  
         <Route path="/menu" element={<Menu />} /> 
         <Route path="/services" element={<Services />} /> {/* Corrected the path */}
         <Route path="/food/:foodName" element={<FoodRecipes />} />
       </Routes>
+      <AboutSection/>
     </Router>
   );
 };
---
+++ Foodism/src/Components/AboutSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AboutSection = () => {
+  return (
+    <div>
+    <div>
+        <p>Customer Favorites</p>
+    </div>
+
+    </div>
+  )
+}
+
+export default AboutSection

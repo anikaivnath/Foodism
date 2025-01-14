069573ae1292aba51848805e394282f793f9d101
anikaivnath <anikaivnath8@gmail.com>
2025-01-14 21:42:19 +0600

    feat(AboutSection): enhance styling with text alignment and add header for popular categories

--- Foodism/src/Components/AboutSection.jsx
+++ Foodism/src/Components/AboutSection.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const AboutSection = () => {
   return (
     <div>
-    <div>
+    <div className="text-center m-4 font-custom">
         <p>Customer Favorites</p>
+        <h1>Popular Catagories</h1>
     </div>
 
     </div>

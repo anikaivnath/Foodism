b80ef69f682e129059b2d0d4292c6fa52c0755fb
anikaivnath <anikaivnath8@gmail.com>
2025-01-14 22:06:30 +0600

    feat(AboutSection): add customer favorites section with images and names

--- Foodism/src/Components/AboutSection.jsx
+++ Foodism/src/Components/AboutSection.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
+import Burger from './image/Burger.png'
+import Cake from './image/Cake.png'
+import Rice from './image/Rice.png'
+import Soup from './image/Soup.png'
+
+const items =[
+   {src:Burger,name:"Burger"},
+   { src:Rice, name:"Rice"},
+   { src:Cake, name:"Cake"},
+   { src:Soup, name:"Soup"}
+]
+
 
 const AboutSection = () => {
   return (
     <div>
     <div className="text-center m-4 font-custom">
         <p>Customer Favorites</p>
-        <h1 className='font-bold text-3xl'>Popular Catagories</h1>
+        <h1 className='font-bold text-3xl mt-4'>Popular Catagories</h1>
+    </div>
+    <div>
+        <div>
+        {
+            items.map((item,index)=>(
+                <div key={index} clas>
+                <img src={item.src} alt={item.name} />
+                <h3>{item.name}</h3>
+                </div>
+
+            ))
+        }
+        
+      
+
+        </div>
     </div>
 
     </div>

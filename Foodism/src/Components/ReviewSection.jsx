3bce6ae997f1950391e3648d665a2de99bcd610f
anikaivnath <anikaivnath8@gmail.com>
2025-01-16 23:24:33 +0600

    fix(ReviewSection): adjust top margin and add spacing for improved layout

--- Foodism/src/Components/ReviewSection.jsx
+++ Foodism/src/Components/ReviewSection.jsx
@@ -12,8 +12,8 @@ const ReviewSection = () => {
             </div>
     </div>
     <div>
-      <div className='  w-[600px] mt-[230px] ml-20 text-justify '>
-        <h1 className='font-bold text-3xl '>What Our Customers Say About Us</h1>
+      <div className='  w-[600px] mt-[210px] ml-20 text-justify '>
+        <h1 className='font-bold text-3xl mt-4'>What Our Customers Say About Us</h1>
         <p className='w-[570px]'>“I had the pleasure of dining at Foodi last night, and I'm still raving 
         about the experience! The attention to detail in presentation and service was 
         impeccable Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni enim similique nulla. Neque non, incidunt dolorem ipsa corrupti soluta laboriosam perspiciatis est eaque harum minima minus autem quidem quam reprehenderit!

bae189e38b112b45798076e9c9de6cac3cab2dfc
anikaivnath <anikaivnath8@gmail.com>
2025-01-16 23:05:23 +0600

    fix(ReviewSection): update div class for improved layout styling

--- Foodism/src/Components/ReviewSection.jsx
+++ Foodism/src/Components/ReviewSection.jsx
@@ -4,7 +4,7 @@ import Rectang from './image/Rectang.png'
 
 const ReviewSection = () => {
   return (
-    <div>
+    <div className='flex j'>
     <div className='relative'>
        <img className='absolute mt-[266px] ml-4 w-80 hidden md:block' src={Rectang} alt='rectang'/>
        <img className='w-80 relative' src={Mask} alt="picture" />

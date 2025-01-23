43f4abf9cea6515b25688721aaf7a1d0e45dd256
anikaivnath <anikaivnath8@gmail.com>
2025-01-23 23:14:29 +0500

    fix(Services): remove unnecessary whitespace in section header

--- Foodism/src/Components/Services.jsx
+++ Foodism/src/Components/Services.jsx
@@ -3,11 +3,8 @@ import React from 'react'
 const Services = () => {
   return (
     <div>
-  
     <h1 className='font-custom text-3xl text-center'>
     Explore Our Latest Services
-p
-
     </h1>
     </div> 
   )

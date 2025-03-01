3cf370faeeb58b2fbb551c82356faf846bd83c55
anikaivnath <anikaivnath8@gmail.com>
2025-03-02 00:55:46 +0500

    feat(App): restructure routing and enhance component organization

--- Foodism/src/App.jsx
+++ Foodism/src/App.jsx
@@ -1,39 +1,26 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Use Routes instead of Switch
-import NavBar from "./NavBar";
-import FoodRecipes from "./FoodRecipes";
-import Cart from "./Cart";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NavBar from "./Components/NavBar";
+import HeroSection from "./Components/HeroSection";
+import Services from "./Components/Services";
+import FoodRecipes from "./Components/FoodRecipes";
+import AboutSection from "./Components/AboutSection";
+import Menu from "./Components/Menu"; 
+import ReviewSection from "./Components/ReviewSection";
 
 const App = () => {
-  const [cart, setCart] = useState([]); // State to store cart items
-
-  // Add item to the cart
-  const addToCart = (meal, price) => {
-    const newCart = [...cart];
-    const existingItem = newCart.find(item => item.idMeal === meal.idMeal);
-
-    if (existingItem) {
-      existingItem.quantity += 1; // If item exists, increase quantity
-    } else {
-      newCart.push({ ...meal, price, quantity: 1 }); // Add new item to cart
-    }
-
-    setCart(newCart);
-  };
-
-  // Calculate total price of the items in the cart
-  const calculateTotal = () => {
-    return cart.reduce((total, item) => total + item.price * item.quantity, 0);
-  };
-
   return (
-    <Router>
-      <NavBar cart={cart} calculateTotal={calculateTotal} />
-      <Routes> {/* Use Routes instead of Switch */}
-        <Route path="/" element={<FoodRecipes addToCart={addToCart} />} />
-        <Route path="/cart" element={<Cart cart={cart} total={calculateTotal()} />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <NavBar />   
+        <Routes>
+          <Route path="/" element={<><HeroSection />  <AboutSection/> <ReviewSection/> </>} />  
+          <Route path="/menu" element={<Menu />} /> 
+          <Route path="/services" element={<Services />} /> {/* Corrected the path */}
+          <Route path="/food/:foodName" element={<FoodRecipes />} />
+        </Routes>
+      </Router>
+    </>
   );
 };
 

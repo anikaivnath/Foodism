b6476fbcb8fcfc15105c89bb448dadc143572985
anikaivnath <anikaivnath8@gmail.com>
2025-04-09 15:47:44 +0500

    fix(JourneySection): adjust layout and enhance 'Explore' paragraph styling for consistency

--- Foodism/src/Components/JourneySection.jsx
+++ Foodism/src/Components/JourneySection.jsx
@@ -2,16 +2,19 @@ import React from 'react'
 const journey = () => {
   return (
     <div>
+    <div className='w-80 '>
     <div>
     <p>Our Story & Services</p>
     <div>
     <h1>Our Culinary Journey And Services</h1>
     </div>
-    <div className='w-80 '>
+    <div>
       <p>Rooted in passion, we curate unforgettable dining experiences and offer exceptional services, blending culinary artistry with warm hospitality.</p>
     </div>
-    <p className=' bg-green-400 w-max p-2 white'>Explore</p>
+    <p className=' bg-green-800 w-max p-2 text-white rounded'>Explore</p>
     </div>
+    </div>
+    
     
     </div>
   )

03b446ae9ef4e5c7170fbc1c7fd37ce56eb33065
anikaivnath <anikaivnath8@gmail.com>
2025-04-09 16:11:52 +0500

    fix(JourneySection): adjust width of catering section and update text styling for consistency

--- Foodism/src/Components/JourneySection.jsx
+++ Foodism/src/Components/JourneySection.jsx
@@ -18,14 +18,14 @@ const journey = () => {
     </div>
     </div>
 
-    <div className=' w-40 justify-center'>
+    <div className=' w-[300px] justify-center'>
       <p className=' text-center text-2xl'>
          <FontAwesomeIcon icon={faMugHot} />
       </p>
       <p className='text-center text-xl'>
         Catering
       </p>
-      <p>Delight your guests with our flavors and  presentation</p>
+      <p className='text-xl'>Delight your guests with our flavors and  presentation</p>
     </div>
     
     

633d331afc1c8bd04cdb21ec2e445916d2cc31ae
anikaivnath <anikaivnath8@gmail.com>
2025-04-09 15:49:55 +0500

    fix(JourneySection): format 'Our Story & Services' paragraph for improved readability

--- Foodism/src/Components/JourneySection.jsx
+++ Foodism/src/Components/JourneySection.jsx
@@ -9,7 +9,9 @@ const journey = () => {
     <h1>Our Culinary Journey And Services</h1>
     </div>
     <div>
-      <p>Rooted in passion, we curate unforgettable dining experiences and offer exceptional services, blending culinary artistry with warm hospitality.</p>
+      <p>Rooted in passion, we curate unforgettable dining experiences 
+      and offer exceptional services, blending culinary artistry with 
+      warm hospitality.</p>
     </div>
     <p className=' bg-green-800 w-max p-2 text-white rounded'>Explore</p>
     </div>

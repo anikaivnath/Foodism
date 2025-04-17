3b491da88848166b402d389ad44cbd997037b7a2
anikaivnath <anikaivnath8@gmail.com>
2025-04-17 15:16:16 +0500

    fix(JourneySection): reformat code structure for improved readability and maintainability

--- Foodism/src/Components/JourneySection.jsx
+++ Foodism/src/Components/JourneySection.jsx
@@ -4,32 +4,32 @@ import { faMugHot, faStar } from '@fortawesome/free-solid-svg-icons';
 const journey = () => {
   return (
     <div>
-    <div className='w-80 '>
-    <div>
-    <p>Our Story & Services</p>
-    <div>
-    <h1>Our Culinary Journey And Services</h1>
-    </div>
-    <div><p>Rooted in passion, we curate unforgettable dining experiences and 
-     offer exceptional services, blending culinary artistry with  
-     warm hospitality.</p>
-    </div>
-    <p className=' bg-green-700 w-max p-2 text-white rounded'>Explore</p>
-    </div>
-    </div>
+      <div className='w-80 '>
+        <div>
+          <p>Our Story & Services</p>
+          <div>
+            <h1>Our Culinary Journey And Services</h1>
+          </div>
+          <div><p>Rooted in passion, we curate unforgettable dining experiences and
+            offer exceptional services, blending culinary artistry with
+            warm hospitality.</p>
+          </div>
+          <p className=' bg-green-700 w-max p-2 text-white rounded'>Explore</p>
+        </div>
+      </div>
 
-    <div className='w-[250px] justify-center text-green-600 shadow-lg p-2'>
-  <p className='text-center text-2xl'>
-    <FontAwesomeIcon icon={faMugHot} />
-  </p>
-  <p className='text-center text-xl'>
-    Catering
-  </p>
-  <p className='pl-2'>Delight your guests with our flavors and presentation</p>
-</div>
+      <div className='w-[250px] justify-center text-green-600 shadow-lg p-2'>
+        <p className='text-center text-2xl'>
+          <FontAwesomeIcon icon={faMugHot} />
+        </p>
+        <p className='text-center text-xl'>
+          Catering
+        </p>
+        <p className='pl-2'>Delight your guests with our flavors and presentation</p>
+      </div>
 
-    
-    
+
+
     </div>
   )
 }

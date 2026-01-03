11ef4dcee4028055d7c480e3d2b2a591ade06d7e
anikaivnath <anikaivnath8@gmail.com>
2026-01-03 20:04:36 +0500

    feat(FoodRecipes): implement FoodRecipes component to fetch and display meal recipes

---
+++ .history/Foodism/src/Components/items_20250326003604.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const FoodRecipes = () => {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://www.themealdb.com/api/json/v1/1/search.php?s=")
+      .then(response => setRecipes(response.data.meals))
+ 
+  }, []);
+  return (
+    <div>
+      <div className="flex">
+      <ul className="flex flex-wrap gap-4">
+        {recipes?.map(meal => (
+          <li key={meal.idMeal}>  
+            <img src={meal.strMealThumb} alt={meal.strMeal} width="200px" />
+            <h3>{meal.strMeal}</h3>
+    
+          </li>
+        ))}
+      </ul>
+
+      </div>
+      
+    </div>
+  );
+};
+
+export default FoodRecipes;
